5ab86cf05078e915e4522d7db2519cccc633089f
acostaLucas <52472619+acostaLucas@users.noreply.github.com>
2019-08-21 22:16:43 -0300

    Add files via upload

---
+++ Clase_01/clase1.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : clase1.c
+ Author      : acosta lucas
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, ANSI-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	puts("hola");  /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
